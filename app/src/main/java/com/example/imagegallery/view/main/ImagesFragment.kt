401d4a09786e5d1c27d42bf8c7e85b306c8f6c7f
auron567 <xauron567x@gmail.com>
2020-07-13 17:42:40 +0200

    Configure layout of ImagesFragment and CollectionFragment

--- app/src/main/java/com/example/imagegallery/view/main/ImagesFragment.kt
+++ app/src/main/java/com/example/imagegallery/view/main/ImagesFragment.kt
@@ -1,10 +1,10 @@
 package com.example.imagegallery.view.main
 
 import android.os.Bundle
+import android.view.*
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import com.example.imagegallery.R
 import com.example.imagegallery.databinding.FragmentImagesBinding
 
 class ImagesFragment : Fragment() {
@@ -16,6 +16,37 @@ class ImagesFragment : Fragment() {
     ): View? {
         val binding = FragmentImagesBinding.inflate(inflater, container, false)
 
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_images, menu)
+        setSearchView(menu)
+    }
+
+    /**
+     * SearchView configuration.
+     */
+    private fun setSearchView(menu: Menu) {
+        val searchItem = menu.findItem(R.id.action_search)
+
+        (searchItem.actionView as SearchView).apply {
+            // Set hint text
+            queryHint = getString(R.string.subreddit)
+
+            // Set listener for user actions
+            setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    // TODO: handle text submit
+                    return true
+                }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    // TODO: handle text change
+                    return true
+                }
+            })
+        }
     }
 }
