722136f2dead43dcba9cea1198b72286312a66cc
auron567 <xauron567x@gmail.com>
2020-07-17 11:29:00 +0200

    Update AGP to 4.0.1

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,5 +1,5 @@
 private object Versions {
-    const val androidGradlePlugin = "4.0.0"
+    const val androidGradlePlugin = "4.0.1"
     const val kotlin = "1.3.72"
     const val coroutines = "1.3.7"
     const val appcompat = "1.1.0"
