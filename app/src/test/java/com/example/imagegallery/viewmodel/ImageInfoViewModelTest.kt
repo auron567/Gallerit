903b930d58afea6ab9089dc928b063a4d9a2c554
auron567 <xauron567x@gmail.com>
2020-08-13 19:40:19 +0200

    Add more unit tests to ImageInfoViewModelTest

--- app/src/test/java/com/example/imagegallery/viewmodel/ImageInfoViewModelTest.kt
+++ app/src/test/java/com/example/imagegallery/viewmodel/ImageInfoViewModelTest.kt
@@ -1,10 +1,16 @@
 package com.example.imagegallery.viewmodel
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
 import com.example.imagegallery.data.repository.RedditImageRepository
 import com.example.imagegallery.utils.CoroutineTestRule
+import com.example.imagegallery.utils.getOrAwaitValue
 import com.example.imagegallery.utils.makeRedditImage
+import io.kotest.matchers.booleans.shouldBeFalse
+import io.kotest.matchers.booleans.shouldBeTrue
 import io.mockk.MockKAnnotations
 import io.mockk.coVerify
+import io.mockk.every
 import io.mockk.impl.annotations.RelaxedMockK
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Before
@@ -14,21 +20,55 @@ import org.junit.Test
 class ImageInfoViewModelTest {
 
     @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
     val coroutineTestRule = CoroutineTestRule()
 
     private lateinit var viewModel: ImageInfoViewModel
     @RelaxedMockK lateinit var repository: RedditImageRepository
 
-    private val image = makeRedditImage()
-
     @Before
     fun setup() {
         MockKAnnotations.init(this)
+    }
+
+    @Test
+    fun isFavorite_returnsTrue() {
+        // Stub repository
+        val image = makeRedditImage()
+        every { repository.isFavorite(image.id) } returns MutableLiveData(true)
+
+        // Call ViewModel
         viewModel = ImageInfoViewModel(repository, image)
+        val liveData = viewModel.isFavorite
+        val value = liveData.getOrAwaitValue()
+
+        // Assertions
+        value.shouldBeTrue()
+    }
+
+    @Test
+    fun isFavorite_returnsFalse() {
+        // Stub repository
+        val image = makeRedditImage()
+        every { repository.isFavorite(image.id) } returns MutableLiveData(false)
+
+        // Call ViewModel
+        viewModel = ImageInfoViewModel(repository, image)
+        val liveData = viewModel.isFavorite
+        val value = liveData.getOrAwaitValue()
+
+        // Assertions
+        value.shouldBeFalse()
     }
 
     @Test
     fun saveImage_callsRepository() = coroutineTestRule.runBlockingTest {
+        // Initialize ViewModel
+        val image = makeRedditImage()
+        viewModel = ImageInfoViewModel(repository, image)
+
         // Call ViewModel
         viewModel.saveImage()
 
@@ -40,6 +80,10 @@ class ImageInfoViewModelTest {
 
     @Test
     fun removeImage_callsRepository() = coroutineTestRule.runBlockingTest {
+        // Initialize ViewModel
+        val image = makeRedditImage()
+        viewModel = ImageInfoViewModel(repository, image)
+
         // Call ViewModel
         viewModel.removeImage()
 
